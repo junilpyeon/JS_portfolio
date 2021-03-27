72b8eb74b76f6d72276146af783dd0c9a19959fb
junilpyeon <junilpyeon@gmail.com>
2021-03-27 13:13:03 +0900

    mangement_system_v1.1

---
+++ study-react-node-master/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="lightpink-background">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop!</h2>
+    </div>
+  );
+}
+
+export default App;
